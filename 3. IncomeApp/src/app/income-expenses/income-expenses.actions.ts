fb8c1a95b3ea5a343ff192056e6a6bb87165af17
JCamiloo <jcamilo.osorio15@gmail.com>
2020-01-07 20:32:15 -0500

    [IncomeApp] adding new reducers - income expenses

---
+++ 3. IncomeApp/src/app/income-expenses/income-expenses.actions.ts
@@ -0,0 +1,15 @@
+import { IncomeExpenses } from './income-expenses.model';
+import { Action } from '@ngrx/store';
+export const SET_ITEMS = '[Income expenses] Set items'
+export const UNSET_ITEMS = '[Income expenses] Unset items'
+
+export class SetItemsAction implements Action {
+  readonly type = SET_ITEMS;
+  constructor(public items: IncomeExpenses[]) {}
+}
+
+export class UnsetItemsAction implements Action {
+  readonly type = UNSET_ITEMS;
+}
+
+export type actions = SetItemsAction | UnsetItemsAction;
---
+++ 3. IncomeApp/src/app/income-expenses/income-expenses.reducer.ts
@@ -0,0 +1,16 @@
+import { IncomeExpenses } from './income-expenses.model';
+import * as incomeExpensesActions from './income-expenses.actions';
+
+export interface IncomeExpensesState { items: IncomeExpenses[]; }
+const initState: IncomeExpensesState = { items: [] };
+
+export function IncomeExpensesReducer (state = initState, action: incomeExpensesActions.actions) {
+  switch (action.type) {
+    case incomeExpensesActions.SET_ITEMS:
+      return { items: [ ...action.items ]};
+    case incomeExpensesActions.UNSET_ITEMS:
+      return { items: [] }; 
+    default:
+      return state;
+  }
+}
