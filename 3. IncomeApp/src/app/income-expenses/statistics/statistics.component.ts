e7840f6d468af60672009ea676f9c868ba4b6923
JCamiloo <jcamilo.osorio15@gmail.com>
2020-01-09 21:42:22 -0500

    [IncomeApp] basic stadistic

--- 3. IncomeApp/src/app/income-expenses/statistics/statistics.component.ts
+++ 3. IncomeApp/src/app/income-expenses/statistics/statistics.component.ts
@@ -1,4 +1,8 @@
+import { IncomeExpenses } from './../income-expenses.model';
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-statistics',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsComponent implements OnInit {
 
-  constructor() { }
+  incomes: number;
+  expenses: number;
+  incomesCounter: number;
+  expensesCounter: number;
+  dataSubscription: Subscription = new Subscription();
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.dataSubscription = this.store.select('incomeExpenses').subscribe(data => {
+      this.countIncomeExpenses(data.items);
+    });
   }
 
+  countIncomeExpenses(items: IncomeExpenses[]) {
+    this.incomes = 0;
+    this.expenses = 0;
+    this.expensesCounter = 0;
+    this.incomesCounter = 0;
+    items.forEach(item => {
+      if (item.type === 'income') {
+        this.incomesCounter++;
+        this.incomes += item.amount;
+      } else {
+        this.expensesCounter++;
+        this.expenses += item.amount;
+      }
+    });
+  }
 }
