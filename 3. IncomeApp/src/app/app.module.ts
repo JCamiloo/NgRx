f42f7f5801fec291077a4f199afe7f354122cdb4
JCamiloo <jcamilo.osorio15@gmail.com>
2020-01-12 19:10:29 -0500

    [IncomeApp] income-expenses module

--- 3. IncomeApp/src/app/app.module.ts
+++ 3. IncomeApp/src/app/app.module.ts
@@ -1,8 +1,8 @@
+import { IncomeExpensesModule } from './income-expenses/income-expenses.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,32 +12,24 @@ import { ChartsModule } from 'ng2-charts';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/components/dashboard/dashboard.component';
-import { IncomeExpensesComponent } from './income-expenses/components/income-expenses/income-expenses.component';
-import { StatisticsComponent } from './income-expenses/components/statistics/statistics.component';
-import { DetailComponent } from './income-expenses/components/detail/detail.component';
-import { OrderIncomesPipe } from './income-expenses/pipes/order-incomes.pipe';
 import { environment } from './../environments/environment';
 import { appReducers } from './app.reducer';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
+import { DashboardComponent } from './dashboard/components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    IncomeExpensesComponent,
-    StatisticsComponent,
-    DetailComponent,
-    OrderIncomesPipe
+    DashboardComponent
   ],
   imports: [
     AuthModule,
+    IncomeExpensesModule,
     SharedModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
     BrowserAnimationsModule,
---
+++ 3. IncomeApp/src/app/income-expenses/income-expenses.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IncomeExpensesComponent } from './components/income-expenses/income-expenses.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { OrderIncomesPipe } from './pipes/order-incomes.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    IncomeExpensesComponent,
+    StatisticsComponent,
+    DetailComponent,
+    OrderIncomesPipe
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    ReactiveFormsModule,
+    ChartsModule    
+  ]
+})
+export class IncomeExpensesModule { }
