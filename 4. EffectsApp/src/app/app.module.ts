20fc5107df9972dbf64c847364a4bd5d16711e23
JCamiloo <jcamilo.osorio15@gmail.com>
2020-01-14 22:06:42 -0500

    [EffectsApp] creating components

--- 4. EffectsApp/src/app/app.module.ts
+++ 4. EffectsApp/src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { UsersModule } from './users/users.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
